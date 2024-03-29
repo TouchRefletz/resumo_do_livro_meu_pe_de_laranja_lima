0096d482156317ccea53a77546a77d8956124133
Willian Campos Costa <willianl2r2@gmail.com>
2024-03-29 15:25:45 -0300

    otimização do site e adição de funcionalidade 'voltar ao topo'

--- app.js
+++ app.js
@@ -119,3 +119,16 @@ function myFunction() {
         x.classList.add('disabled');
     }
 }
+let mybutton = document.querySelector('.botao');
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
